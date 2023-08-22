b599eb99f3a99d571d0439def9738f9f458cd3dd
Abhishek Chauhan <abhishek01web@gmail.com>
2023-08-22 22:24:49 +0530

    latest commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Your NODE App is live - reactjsweb.tech');
 });
 
 server.listen(port, hostname, () => {

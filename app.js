c944a45c2311d41fdab4b3d396c2712bcb2ab085
Abhishek Chauhan <abhishek01web@gmail.com>
2023-08-22 22:11:43 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -12,3 +12,11 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// echo "# nodeapp" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Abhishek01web/nodeapp.git
+// git push -u origin main
